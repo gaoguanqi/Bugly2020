a65c4333a101ffb637938e25e0e73a7b728ef4cc
gaoguanqi <307590625@qq.com>
2020-07-30 17:08:07 +0800

    test

--- app/src/main/java/com/maple/bugly/view/fragment/MainFragment.java
+++ app/src/main/java/com/maple/bugly/view/fragment/MainFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,8 @@ import com.maple.bugly.R;
 import com.maple.bugly.app.base.BaseFragment;
 
 public class MainFragment extends BaseFragment{
+
+    private TextView tvContent;
 
     public static MainFragment getInstance(){
         MainFragment fragment = new MainFragment();
@@ -23,5 +26,12 @@ public class MainFragment extends BaseFragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvContent = view.findViewById(R.id.tv_content);
+        tvContent.setText("下发补丁测试");
     }
 }
